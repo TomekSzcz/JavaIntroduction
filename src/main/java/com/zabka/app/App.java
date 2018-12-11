881ac7cec40a417e7c1c7be116e8d698e75c3838
tszczesn <tomasz.szczesniak11@gmail.com>
2018-12-11 12:16:10 +0100

    Repo Init

---
+++ src/main/java/com/zabka/app/App.java
@@ -0,0 +1,20 @@
+package com.zabka.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.zabka.app")
+@SpringBootApplication
+@EnableScheduling
+public class App
+{
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
